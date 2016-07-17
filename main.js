5073561c9d46dee913ffb28ceb877293361a4c00
Baptiste Bertrand-Rapello <baptiste2394@gmail.com>
2016-07-17 17:28:38 +0200

    avec un console log

--- main.js
+++ main.js
@@ -45,6 +45,11 @@ module.exports.loop = function () {
     if (harvester < nbHarvester) {
         Game.spawns["Spawn1"].createCreep([MOVE, CARRY, WORK, WORK], undefined, {role:"harvester"});
     }
-
-
+    if (builder < nbBuilder) {
+        Game.spawns["Spawn1"].createCreep([MOVE, CARRY, WORK, WORK], undefined, {role:"builder"});
+    }
+    if (harvester < nbUpgrader) {
+        Game.spawns["Spawn1"].createCreep([MOVE, CARRY, WORK, WORK], undefined, {role:"upgrader"});
+    }
+    console.log("nb harvester : " + harvester +  "; nb builder : " + builder + "; nbupgrader : " + upgrader)
 }
