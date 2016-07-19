3f9e2c7b95ebca2ad1b9e81f4473984edfc3f7d4
Baptiste Bertrand-Rapello <baptiste2394@gmail.com>
2016-07-19 11:00:34 +0200

    on va recommencer ...

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ var roleBuilder = require('role.builder');
 
 module.exports.loop = function () {
 
-    var nbHarvester = 4;
-    var nbBuilder = 4;
-    var nbUpgrader = 5;
+    var nbHarvester = 3;
+    var nbBuilder = 0;
+    var nbUpgrader = 0;
     var harvester = 0;
     var upgrader = 0;
     var builder = 0;
