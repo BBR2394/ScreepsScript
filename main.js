fcdefcdfd9de1a59c724c06c7bfaeee6cafcc9e0
Baptiste Bertrand-Rapello <baptiste2394@gmail.com>
2016-07-20 09:14:30 +0200

    change number and builder build tower

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ var roleBuilder = require('role.builder');
 module.exports.loop = function () {
 
     var nbHarvester = 7;
-    var nbBuilder = 2;
-    var nbUpgrader = 7;
+    var nbBuilder = 4;
+    var nbUpgrader = 5;
     var harvester = 0;
     var upgrader = 0;
     var builder = 0;
