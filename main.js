31fa0873d5dec66bb4e906e668af55666871173a
Baptiste Bertrand-Rapello <baptiste2394@gmail.com>
2016-07-22 18:48:23 +0200

    c null

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ var roleAttacker = require('role.attacker');
 
 module.exports.loop = function () {
 
-    var nbHarvester = 2;
-    var nbBuilder = 3;
-    var nbUpgrader = 1;
+    var nbHarvester = 1;
+    var nbBuilder = 0;
+    var nbUpgrader = 0;
     var nbAttacker = 0;
     var harvester = 0;
     var upgrader = 0;
